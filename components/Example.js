d5bfdc766c57141818ced4cd79b234293ebfb131
ktw1016 <ktw_1016@hotmail.com>
2019-03-07 21:31:37 -0500

    example of both get and post for our nodejs express server

--- components/Example.js
+++ components/Example.js
@@ -5,20 +5,35 @@ class Example extends Component {
    state = {
       data: ''
    }
-   componentDidMount = () => {
-      fetch('http://localhost:8080/api/users', {
+
+   componentDidMount(){
+      fetch('http://localhost:8080/api/postUsers', {
          method: 'POST',
          headers: {
-           Accept: 'application/json',
-           'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
          },
          body: JSON.stringify({
-           username: 'owen',
-           password: 'pass',
+            username: 'owen',
+            password: 'pass',
          }),
 
       });
-   }
+      
+      fetch('http://localhost:8080/api/getUsers')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            data: responseJson.password
+          }, function(){
+  
+          });
+  
+        })
+        .catch((error) =>{
+          console.error(error);
+        });
+    }
 
 
 
@@ -27,7 +42,7 @@ class Example extends Component {
       return (
          <View>
             <Text>
-               {this.state.data.body}
+               {this.state.data}
             </Text>
          </View>
       )
