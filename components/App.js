1abcbbb089096ee4f13fb26fc8d8661625201fdb
Anushka Paliwal <paliwal.anushka@gmail.com>
2019-03-21 16:42:05 -0400

    Search bar; TODO: center icon

--- components/App.js
+++ components/App.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import {createSwitchNavigator, createDrawerNavigator, createStackNavigator, createAppContainer} from 'react-navigation';
+import React, { Component } from "react";
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  createSwitchNavigator,
+  createDrawerNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
 
-import Register from './Register.js';
-import ContinueWithPassword from './ContinueWithPassword.js';
-import CreateAccount from './CreateAccount.js';
-import Home from './Home.js';
-import Service from './Service.js';
-import ServicePreview from './ServicePreview.js';
-import ViewAccount from './ViewAccount.js';
-import BecomeASeller from './BecomeASeller.js';
-import AuthLoadingScreen from './AuthLoadingScreen.js';
-
+import Register from "./Register.js";
+import ContinueWithPassword from "./ContinueWithPassword.js";
+import CreateAccount from "./CreateAccount.js";
+import Home from "./Home.js";
+import Service from "./Service.js";
+import ServicePreview from "./ServicePreview.js";
+import ViewAccount from "./ViewAccount.js";
+import BecomeASeller from "./BecomeASeller.js";
+import AuthLoadingScreen from "./AuthLoadingScreen.js";
 
 class NavigationDrawerStructure extends Component {
   //Structure for the navigatin Drawer
@@ -20,21 +24,17 @@ class NavigationDrawerStructure extends Component {
   };
   render() {
     return (
-      <View style={{ flexDirection: 'row' }}>
+      <View style={{ flexDirection: "row" }}>
         <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
-
           <Image
-            source={require('../image/drawer.png')}
+            source={require("../image/drawer.png")}
             style={{ width: 25, height: 25, marginLeft: 5 }}
           />
-
         </TouchableOpacity>
       </View>
     );
   }
 }
-
-
 
 const DrawerNavigatorExample = createDrawerNavigator({
   //Drawer Optons and indexing
@@ -42,108 +42,109 @@ const DrawerNavigatorExample = createDrawerNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
-      drawerLabel: 'Home',
-    },
+      drawerLabel: "Home"
+    }
   },
   ViewAccount: {
     screen: ViewAccount,
     navigationOptions: {
-      drawerLabel: 'Your Account',
-    },
-  },
-
+      drawerLabel: "Your Account"
+    }
+  }
 });
 
 const AuthStack = createStackNavigator({
   Register: {
     screen: Register,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
+      headerTintColor: "#000000"
+    })
   },
   ContinueWithPassword: {
     screen: ContinueWithPassword,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
+      headerTintColor: "#000000"
+    })
   },
   CreateAccount: {
     screen: CreateAccount,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
-  },
-})
+      headerTintColor: "#000000"
+    })
+  }
+});
 
 const AppStack = createStackNavigator({
   Home: {
     screen: DrawerNavigatorExample,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
+      headerTintColor: "#000000"
+    })
   },
   Service: {
     screen: Service,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
+      headerTintColor: "#000000"
+    })
   },
   ViewAccount: {
     screen: ViewAccount,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
+      headerTintColor: "#000000"
+    })
   },
   BecomeASeller: {
     screen: BecomeASeller,
     navigationOptions: ({ navigation }) => ({
-      title: 'Servus',
+      title: "Servus",
       //headerLeft: <NavigationDrawerStructure navigationProps={navigation} />,
       headerStyle: {
-        backgroundColor: '#ffffff',
+        backgroundColor: "#ffffff"
       },
-      headerTintColor: '#000000',
-    }),
-  },
+      headerTintColor: "#000000"
+    })
+  }
 });
 
-const switchNavigator = createSwitchNavigator({
-  AuthLoading: AuthLoadingScreen,
-  App: AppStack,
-  Auth: AuthStack,
-},
-{
-  initialRouteName: 'AuthLoading',
-})
+const switchNavigator = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: "AuthLoading"
+  }
+);
 
 export default createAppContainer(switchNavigator);
--- components/Home.js
+++ components/Home.js
@@ -1,37 +1,89 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, AsyncStorage} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Button, Input} from 'react-native-elements';
-import ServicePreview from './ServicePreview.js';
+import React, { Component } from "react";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  StatusBar
+} from "react-native";
+import Icon from "react-native-vector-icons/EvilIcons";
+import ServicePreview from "./ServicePreview.js";
 
 class CreateAccount extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: '',
-      username: '',
+      password: "",
+      username: ""
+    };
+  }
+
+  componentWillMount() {
+    this.startHeaderHeight = 80;
+    if (Platform.OS == "android") {
+      this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
   }
 
-
   render() {
     const { navigation } = this.props;
-    const firstName = JSON.parse(JSON.stringify(navigation.getParam('firstName', 'NO-NAME')));
-    const id = JSON.parse(JSON.stringify(navigation.getParam('id', 'NO-NAME')));
-    AsyncStorage.getItem('userId', (err, result) => {
+    const firstName = JSON.parse(
+      JSON.stringify(navigation.getParam("firstName", "NO-NAME"))
+    );
+    const id = JSON.parse(JSON.stringify(navigation.getParam("id", "NO-NAME")));
+    AsyncStorage.getItem("userId", (err, result) => {
       //alert(result);
     });
     return (
-      <View style={st.container}>
-        <Text style={st.heading2}> Welcome to Servus Home Page</Text>
-        <ServicePreview navigation={navigation}/>
-      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={st.container}>
+          <View
+            style={{
+              height: this.startHeaderHeight,
+              backgroundColor: "white",
+              borderBottomWidth: 1,
+              borderBottomColor: "#dddddd"
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                padding: 10,
+                backgroundColor: "white",
+                marginHorizontal: 20,
+                shadowOffset: { width: 0, height: 0 },
+                shadowColor: "black",
+                shadowOpacity: 0.2,
+                elevation: 1,
+                marginTop: Platform.OS == "android" ? 25 : null
+              }}
+            >
+              <Icon name="search" size={40} />
+              <TextInput
+                underlineColorAndroid="transparent"
+                placeholder="What are you looking for?"
+                placeholderTextColor="lightgrey"
+                style={{
+                  flex: 1,
+                  fontWeight: "300",
+                  fontSize: 20,
+                  backgroundColor: "white"
+                }}
+              />
+            </View>
+          </View>
+          <Text style={st.heading2}> Welcome to Servus </Text>
+          <ServicePreview navigation={navigation} />
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
-const st = require('./style');
-const styles = StyleSheet.create({
-});
+const st = require("./style");
+const styles = StyleSheet.create({});
 export default CreateAccount;
